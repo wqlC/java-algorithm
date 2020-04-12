930df60d0735dd9c02d2e30b7eeae8d22c9ccd25
tailor <tailor@nwpu.com>
2020-04-13 00:03:42 +0800

    leetCode

--- 15-3Sum/src/Solution.java
+++ 15-3Sum/src/Solution.java
@@ -27,37 +27,36 @@ class Solution {
     }
     // 双指针
     public List<List<Integer>> threeSum2(int[] nums) {
-        // 双指针
-        public List<List<Integer>> threeSum(int[] nums) {
-            List<List<Integer>> res = new ArrayList<>();
-            if(nums == null || nums.length<2){
-                return res;
-            }
 
-            Arrays.sort(nums);// 排序数据
-            for (int i = 0; i < nums.length; i++) {
-                if(nums[i]>0) break; // 数组排过序的
-                if(i>0 && nums[i] == nums[i-1]) continue;// 去重
-                int l = i+1;
-                int r = nums.length-1;
-                while (l < r){
-                    int temp = nums[i] + nums[l] + nums[r];
-                    if(temp == 0){
-                        res.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                        while(l<r && nums[l] == nums[l+1]) l++; // 去重
-                        while(l<r && nums[r] == nums[r-1]) r--;	// 去重
-                        l++;
-                        r--;
-                    }else if(temp<0){
-                        l++;
-                    }else {
-                        r--;
-                    }
-                }
-            }
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums == null || nums.length<2){
             return res;
         }
+
+        Arrays.sort(nums);// 排序数据
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]>0) break; // 数组排过序的
+            if(i>0 && nums[i] == nums[i-1]) continue;// 去重
+            int l = i+1;
+            int r = nums.length-1;
+            while (l < r){
+                int temp = nums[i] + nums[l] + nums[r];
+                if(temp == 0){
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while(l<r && nums[l] == nums[l+1]) l++; // 去重
+                    while(l<r && nums[r] == nums[r-1]) r--;	// 去重
+                    l++;
+                    r--;
+                }else if(temp<0){
+                    l++;
+                }else {
+                    r--;
+                }
+            }
+        }
+        return res;
     }
+
     public static void main(String[] args) {
         int[] nums = {-1, 0, 1, 2, -1, -4};
         List<List<Integer>> res = new Solution().threeSum2(nums);
